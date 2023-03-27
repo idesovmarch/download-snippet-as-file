99a091b0f0a663521398a41c9b1c930194d48a37
idesovmarch <bdhammer2288@gmail.com>
2023-03-27 18:06:51 -0400

    added favicon credit

--- src/App.js
+++ src/App.js
@@ -12,13 +12,18 @@ function App() {
 
       <Card className='infoCard'>
         <Card.Body>
-          Paste your JavaScript in the box below and click the button to download a PNG or SVG of the box below
-          Useful for tutorials and documentation
+          Paste your JavaScript in the box below and click the button to download a PNG or SVG of
+          the box below Useful for tutorials and documentation
         </Card.Body>
       </Card>
 
       <TextBox />
-      <footer className='footerBar'>Brad Hammer "Idesovmarch" 2023</footer>
+      <footer className='footerBar'>
+        Brad Hammer "Idesovmarch" 2023{' '}
+        <a href='https://www.flaticon.com/free-icons/save' title='save icon favicon'>
+          Save icon used as favicon designed by designhub - Flaticon
+        </a>
+      </footer>
     </div>
   );
 }
